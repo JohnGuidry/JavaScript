f8c38ca07cfb85c5dd700595db78233c71fe0729
Snipererer <john.guidry92@gmail.com>
2019-05-20 19:19:10 -0400

    chapter2: Went over variables and wrote qUnit test cases for variables.

---
+++ chapter2/variables.js
@@ -0,0 +1,18 @@
+var score;
+score = 100;
+console.log("Your score was " + score);
+console.log("Great splat!!");
+score = score + 50;
+console.log("New score: " + score);
+console.log("Way to go!");
+
+var message = "Hello World!"; // This is how we could use qUnit in chapter 1 for validation.
+console.log(message); 
+
+message = 'Congratulations! Your tweet won a prize...'
+console.log(message); 
+
+var playerName = "Kandra";
+var locationName = "The Dungeon of Doom";
+
+console.log(playerName + " is in " + locationName);
